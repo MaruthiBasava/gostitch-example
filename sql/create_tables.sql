64299c28b67f5b649d5a3a53651b628a0a62495e
Maruthi Basava <mbasava@Maruthis-MacBook-Pro.local>
2019-02-01 11:57:37 -0500

    added new datasets

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,6 +1,25 @@
 
 
-create table pet_owners (
-    pet_owner_id uuid,
+create table dog_intelligence(
+    dog_intel_id uuid default generate_uuid_v4(),
+    breed varchar(50) not null,
+    classification varchar(100) not null,
+    obey float,
+    reps_lower int,
+    reps_higher int,
+    primary key(dog_intel_id),
+    unique(breed)
 );
+
+create table dog_breeds(
+    dog_breed_id uuid default generate_uuid_v4(),
+    breed varchar(50) not null,
+    height_low_inches int, 
+    height_high_inches int,
+    weight_low_lbs int,
+    weight_high_lbs int,
+    primary key (dog_breed_id),
+    unique(breed)
+);
+
 
